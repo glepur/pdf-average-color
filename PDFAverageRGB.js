ae34bca8c47fca41f3a8e084fc32e0974b6da61c
Goran Lepur <goran.lepur1@gmail.com>
2015-11-11 18:19:57 +0100

    main commit

---
+++ PDFAverageRGB.js
@@ -0,0 +1,66 @@
+(function (window, document, undefined) {
+
+    'use strict';
+
+    window.PDFAverageRGB = function (file, callback, options) {
+
+        var options = options || {},
+            pageNumber = options.pageNumber || 1, 
+            blockSize = options.sampling || 100,
+            viewportScale = options.viewportScale || 1,
+            defaultRGB = options.defaultColor || {r:0,g:0,b:0},
+            canvas = options.canvas || document.createElement('canvas'),
+            context = canvas.getContext && canvas.getContext('2d'),
+            data,
+            i = -4,
+            length,
+            rgb = {r:0,g:0,b:0},
+            count = 0;
+            
+        if (!context) {
+            return defaultRGB;
+        }
+        
+        PDFJS.getDocument(file).then(function(pdf) {
+
+            pdf.getPage(pageNumber).then(function(page) {
+                var viewport = page.getViewport(viewportScale);
+
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+
+                var renderContext = {
+                  canvasContext: context,
+                  viewport: viewport
+                };
+                page.render(renderContext).then(function () {
+                    try {
+                        data = context.getImageData(0, 0, canvas.width, canvas.height);
+                    } catch(e) {
+                        /* security error, img on diff domain */alert('x');
+                        return defaultRGB;
+                    }
+                    
+                    length = data.data.length;
+                    
+                    while ( (i += blockSize * 4) < length ) {
+                        ++count;
+                        rgb.r += data.data[i];
+                        rgb.g += data.data[i+1];
+                        rgb.b += data.data[i+2];
+                    }
+                    
+                    // ~~ used to floor values
+                    rgb.r = ~~(rgb.r/count);
+                    rgb.g = ~~(rgb.g/count);
+                    rgb.b = ~~(rgb.b/count);
+
+                    callback(rgb);
+                });
+
+            });
+
+        });
+    }
+
+})(window, document);
